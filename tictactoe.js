6dcf328d72291fcf9a57ef0120dd210d032f1dc3
Brianna Guarino <guarino.bb@gmail.com>
2021-10-15 08:39:01 -0500

    change gridlines

--- tictactoe.js
+++ tictactoe.js
@@ -1,9 +1,24 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
 
-const Gameboard = (() => {
-  let gameboard = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
-
-const getGameboard = () => {
-  return gameboard;
+const drawBoard = () => {
+  boxes.forEach((box, index) => {
+    let styleString = '';
+    if (index < 3) {
+      styleString += `border-bottom: 7px solid #fee440;`;
+    }
+    if (index % 3 === 0) {
+      styleString += `border-right: 7px solid #fee440;`;
+    }
+    if (index % 3 === 2) {
+      styleString += `border-left: 7px solid #fee440;`;
+    }
+    if (index > 5) {
+      styleString += `border-top: 7px solid #fee440;`;
+    }
+    box.style = styleString;
+  })
 }
-}) 
+
+drawBoard();
+
+fee440
